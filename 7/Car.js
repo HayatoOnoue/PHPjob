c99c739f04b0c243e2c39a584ebb0a7282f1b7f7
HayatoOnoue <50014398+HayatoOnoue@users.noreply.github.com>
2023-01-18 13:47:19 +0900

    7章チェックテスト

---
+++ 7/Car.js
@@ -0,0 +1,13 @@
+class Car {
+  constructor(gas, number) {
+    this.gas = gas;
+    this.number = number;
+  }
+  
+  getNumGas() {
+    console.log(`ガソリンは${this.gas}です。ナンバーは${this.number}です`);
+  }
+}
+
+let car = new Car('レギュラー', '1234');
+car.getNumGas();
---
+++ 7/even.js
@@ -0,0 +1,11 @@
+let numbers = [2, 5, 12, 13, 15, 18, 22];
+//ここに答えを実装してください。↓↓↓
+function isEven(num) {
+  if (num % 2 === 0) {
+    console.log(num + 'は偶数です');
+  }
+}
+
+for (let i = 0; i < numbers.length; i++) {
+  isEven(numbers[i]);
+}
