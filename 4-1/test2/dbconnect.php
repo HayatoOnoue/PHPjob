adda63f241860e3c779553238b9017886563794e
HayatoOnoue <50014398+HayatoOnoue@users.noreply.github.com>
2023-01-20 02:21:08 +0900

    4章 バグ修正を行う

---
+++ 4-1/test2/dbconnect.php
@@ -0,0 +1,14 @@
+<?php
+// セッション開始
+// ここでのセッション開始は不要？
+// session_start();
+// DBサーバのURL
+$db['host'] = "localhost";
+// ユーザー名
+$db['user'] = "root";
+// ユーザー名のパスワード
+// $db['pass'] = "";
+$db['pass'] = "root";
+// データベース名
+// $db['dbname'] = "loginManagement";
+$db['dbname'] = "YIGroupBlog";
